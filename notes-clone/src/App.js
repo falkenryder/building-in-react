9edb641d1b93933fcd13f58382a57622a06ca000
Kenneth Chien <kennethchien.kc@gmail.com>
2023-01-20 08:13:13 +0800

    Basic setup

--- notes-clone/src/App.js
+++ notes-clone/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Split from 'react-split';
+import {nanoid} from "nanoid";
+// import date from 'date-and-time';
+import Sidebar from './components/Sidebar'
+import Editor from './components/Editor';
 
-function App() {
+export default function App() {
+  const [notes, setNotes] = React.useState([])
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ""
+  )
+
+  const createNewNote = () => {
+    const newNote = {
+      id: nanoid(),
+      body: ""
+      // lastUpdated: date.format(new Date(), 'ddd, MMM DD YYYY')
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+
+  const updateNote = (text) => {
+    setNotes(prevNotes => prevNotes.map(note => {
+      console.log(note.id === currentNoteId)
+      return note.id === currentNoteId ? {...note, body: text} : note
+    }))
+  }
+
+  const findCurrentNote = () => {
+    return notes.find(note => {
+      return note.id === currentNoteId
+    }) || notes[0]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main>
+      {
+        notes.length > 0 ?
+        <Split
+          sizes={[30,70]}
+          direction="horizontal"
+          className='split'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {
+            currentNoteId && notes.length > 0 &&
+            <Editor
+              currentNote={findCurrentNote()}
+              updateNote={updateNote}
+            />
+          }
+      </Split> :
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button
+            className="first-note"
+            onClick={createNewNote}
+          >
+            Create new note
+          </button>
+        </div>
+      }
+    </main>
   );
 }
-
-export default App;
