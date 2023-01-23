02024a99e8159a60f612be630abcddd63ce36b3d
Kenneth Chien <kennethchien.kc@gmail.com>
2023-01-23 11:28:30 +0800

    added essentials

--- notes-clone/src/App.js
+++ notes-clone/src/App.js
@@ -7,32 +7,49 @@ import Sidebar from './components/Sidebar'
 import Editor from './components/Editor';
 
 export default function App() {
-  const [notes, setNotes] = React.useState([])
+  const [notes, setNotes] = React.useState(() =>
+    JSON.parse(localStorage.getItem("notes")) || []
+  )
+
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ""
   )
 
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
+
   const createNewNote = () => {
     const newNote = {
       id: nanoid(),
-      body: ""
-      // lastUpdated: date.format(new Date(), 'ddd, MMM DD YYYY')
+      body: "",
+      lastUpdated: Date.now()
     }
     setNotes(prevNotes => [newNote, ...prevNotes])
     setCurrentNoteId(newNote.id)
   }
 
   const updateNote = (text) => {
-    setNotes(prevNotes => prevNotes.map(note => {
-      console.log(note.id === currentNoteId)
-      return note.id === currentNoteId ? {...note, body: text} : note
-    }))
+    setNotes(prevNotes => {
+      let arr = []
+      prevNotes.forEach(note =>
+        note.id === currentNoteId ?
+        arr.unshift({...note, body: text, lastUpdated: Date.now()}) :
+        arr.push(note)
+      )
+      return arr
+    })
   }
 
   const findCurrentNote = () => {
     return notes.find(note => {
       return note.id === currentNoteId
     }) || notes[0]
+  }
+
+  const deleteNote = (event, noteId) => {
+    event.stopPropagation()
+    setNotes(prevNotes => prevNotes.filter(note => note.id !== noteId))
   }
 
   return (
@@ -49,6 +66,7 @@ export default function App() {
             currentNote={findCurrentNote()}
             setCurrentNoteId={setCurrentNoteId}
             newNote={createNewNote}
+            deleteNote={deleteNote}
           />
           {
             currentNoteId && notes.length > 0 &&
--- notes-clone/src/components/Sidebar.js
+++ notes-clone/src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React from "react"
+import date from 'date-and-time';
 
 export default function Sidebar(props) {
-  const notesEl = props.notes.map((note, idx) => (
+  const notesEl = props.notes.map((note) => (
     <div key={note.id}>
       <div
         className={`title ${note.id === props.currentNote.id ?
@@ -9,7 +10,19 @@ export default function Sidebar(props) {
         }`}
         onClick={() => props.setCurrentNoteId(note.id)}
       >
-        <h4 className="text-snippet">Note #{idx + 1}</h4>
+        <div><h4 className="text-snippet">
+          {note.body === "" ? "New Note" : note.body.split("\n")[0]}
+        </h4></div>
+        <div className="time"><p>
+          { date.format(new Date(), 'D/M/YY') === date.format(new Date(note.lastUpdated), 'D/M/YY') ?
+          date.format(new Date(note.lastUpdated), 'hh:mm A') :
+          date.format(new Date(note.lastUpdated), 'D/M/YY')
+          }
+        </p>
+        <p style={{opacity: "0.7"}}>
+          {note.body.split("\n")[1] && note.body.split("\n")[1]}
+        </p>
+        </div>
       </div>
     </div>
   ))
@@ -17,9 +30,13 @@ export default function Sidebar(props) {
   return (
     <section className="pane sidebar">
       <div className="sidebar-header">
-        <button className="new-note" onClick={props.newNote}>
+        <button className="button" onClick={(event) => props.deleteNote(event, props.currentNote.id)}>
+          <i className="fa-solid fa-trash-can"></i>
+          </button>
+        <button className="button" onClick={props.newNote}>
           <i className="fa-solid fa-pen-to-square"></i>
           </button>
+
       </div>
       {notesEl}
     </section>
